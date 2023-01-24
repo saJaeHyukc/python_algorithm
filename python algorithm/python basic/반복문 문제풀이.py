8d9d931a03c8013e8e770428e6760ad68b2a0e07
ssa <wogur981208@gmail.com>
2023-01-24 15:12:53 +0900

    Add 반복문 문제 풀이

---
+++ python algorithm/python basic/반복문 문제풀이.py
@@ -0,0 +1,26 @@
+'''
+반복문을 이용한 문제풀이
+1) 1부터 N까지 홀수 출력하기
+2) 1부터 N까지 합 구하기
+3) N의 약수출력하기
+'''
+
+
+# 1번 문제
+n = int(input())
+for i in range(1, n+1):
+    if i % 2 == 1:
+        print(i)
+        
+# 2번 문제
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    sum += i
+print(sum)
+
+# 3번 문제
+n = int(input())
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i, end=' ')
