8aecd9315bb27badb7d57d10e08170bcd29d15b4
ssa <wogur981208@gmail.com>
2023-01-24 20:10:50 +0900

    Add programmers 2023-01-24

---
+++ python algorithm/programmers/0_level/2023-01-24 (1일차).py
@@ -0,0 +1,23 @@
+'''
+1. 두 수의 합
+'''
+def solution(num1, num2):
+    return num1 + num2
+
+'''
+2.  두 수의 차
+'''
+def solution(num1, num2):
+    return num1 - num2
+
+'''
+3. 두 수의 곱
+'''
+def solution(num1, num2):
+    return num1 * num2
+
+'''
+4. 몫 구하기
+'''
+def solution(num1, num2):
+    return num1 // num2
---
+++ python algorithm/programmers/1_level/2023-01-24(1일차).py
@@ -0,0 +1,12 @@
+'''
+약수의 합
+'''
+def solution(n):
+    cnt = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            cnt += i
+    return cnt
+
+print(solution(12))
+print(solution(5))
