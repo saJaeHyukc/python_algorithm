f6d79cd62a625b2d0fcb45762da31c5ac69dacc4
ssa <wogur981208@gmail.com>
2023-01-26 12:56:39 +0900

    Add 2023-01-26 (3일차) 프로그래머스 0레벨

---
+++ python algorithm/programmers/0_level/2023-01-26 (3일차).py
@@ -0,0 +1,36 @@
+'''
+1. 나머지 구하기
+'''
+def solution(num1, num2):
+    return num1 % num2
+
+'''
+2. 중앙값 구하기
+'''
+def solution(array):
+    array.sort()
+    return array[len(array) // 2]
+
+'''
+3. 최빈값 구하기
+생각
+#1. 최빈값을 담은 딕셔너리를 구함
+#2. 딕셔너리의 벨류 값 중 최댓값과 같은 것만 따로 리스트를 만들어 담음
+#3. 리스트 요소가 1개일 때 그 리스트 요소 반환, 아닐 때 -1 반환
+'''
+def solution(array):
+    count = {}
+    for i in array:
+        try:
+            count[i] += 1
+        except:
+            count[i] = 1     
+    
+    modes = []
+    for k, v in count.items():
+        if v == max(count.values()):
+            modes.append(k)
+    if len(modes) == 1:
+        return modes[0]
+    else:
+        return -1
